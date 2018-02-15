cbc8e55eeca3826421d57051585edaafaaada863
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-02-15 18:33:35 +0200

    Beeping widget

--- kucher/view/main_window/dashboard_widget/control_widget/misc_control_widget.py
+++ kucher/view/main_window/dashboard_widget/control_widget/misc_control_widget.py
@@ -12,8 +12,9 @@
 # Author: Pavel Kirienko <pavel.kirienko@zubax.com>
 #
 
-from PyQt5.QtWidgets import QWidget
-from view.device_model_representation import Commander, GeneralStatusView
+from PyQt5.QtWidgets import QWidget, QDoubleSpinBox, QLabel
+from view.device_model_representation import Commander, GeneralStatusView, TaskID
+from view.utils import make_button, lay_out_horizontally, lay_out_vertically
 from .base import SpecializedControlWidgetBase
 
 
@@ -25,8 +26,51 @@ class MiscControlWidget(SpecializedControlWidgetBase):
 
         self._commander = commander
 
+        self._frequency_input = QDoubleSpinBox(self)
+        self._frequency_input.setRange(100, 15000)
+        self._frequency_input.setValue(3000)
+        self._frequency_input.setSuffix(' Hz')
+        self._frequency_input.setToolTip('Beep frequency, in hertz')
+        self._frequency_input.setStatusTip(self._frequency_input.toolTip())
+
+        self._duration_input = QDoubleSpinBox(self)
+        self._duration_input.setRange(0.01, 3)
+        self._duration_input.setValue(0.5)
+        self._duration_input.setSuffix(' s')
+        self._duration_input.setToolTip('Beep duration, in seconds')
+        self._duration_input.setStatusTip(self._duration_input.toolTip())
+
+        self._go_button = make_button(self,
+                                      text='Beep',
+                                      icon_name='speaker',
+                                      tool_tip='Sends a beep command to the device once',
+                                      on_clicked=self._beep_once)
+
+        self.setLayout(
+            lay_out_vertically(
+                lay_out_horizontally(
+                    QLabel('Frequency', self),
+                    self._frequency_input,
+                    QLabel('Duration', self),
+                    self._duration_input,
+                    self._go_button,
+                    (None, 1)
+                ),
+                (None, 1)
+            )
+        )
+
     def stop(self):
         pass
 
     def on_general_status_update(self, timestamp: float, s: GeneralStatusView):
-        pass
+        if s.current_task_id in (TaskID.BEEPING,
+                                 TaskID.IDLE,
+                                 TaskID.FAULT):
+            self.setEnabled(True)
+        else:
+            self.setEnabled(False)
+
+    def _beep_once(self):
+        self._launch_async(self._commander.beep(frequency=self._frequency_input.value(),
+                                                duration=self._duration_input.value()))
