d6c3364b4d2da737599005cd2e4fda7f80d6cc4e
loicgilbert <loic.gilbert3@gmail.com>
2019-06-19 18:06:27 +0300

    test-CI

---
+++ test.bat
@@ -0,0 +1,6 @@
+start python zubax-kucher
+echo ok
+timeout 15
+tasklist /fi "windowtitle eq Zubax Kucher"|find ":" > nul
+echo %errorlevel%
+if errorlevel 1 taskkill /fi "windowtitle eq Zubax Kucher" exit 1
