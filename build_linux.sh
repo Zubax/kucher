2fd592014ae6b6ff058ee4d7f71963aa479394c3
loicgilbert <loic.gilbert3@gmail.com>
2019-07-05 12:38:52 +0300

    Fix  staticx

--- build_linux.sh
+++ build_linux.sh
@@ -7,6 +7,7 @@ sudo apt-get install patchelf -y
 
 pyinstaller --clean --noconfirm pyinstaller.spec || exit 2
 
+sudo apt-get install -y liblzma-dev
 pip install backports.lzma
 
 # https://github.com/JonathonReinhart/staticx/issues/79
