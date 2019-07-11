7eda1894ec352525b45916ab833bbcae2ddca0e3
loicgilbert <loic.gilbert3@gmail.com>
2019-07-11 14:53:15 +0300

    Add blank line

--- kucher/view/main_window/telega_control_widget/control_widget/run_control_widget.py
+++ kucher/view/main_window/telega_control_widget/control_widget/run_control_widget.py
@@ -59,6 +59,7 @@ class RunControlWidget(SpecializedControlWidgetBase):
         self._setpoint_control.status_tip = self._setpoint_control.tool_tip
         self._setpoint_control.value_change_event.connect(self._on_setpoint_changed)
         self._setpoint_control.spinbox.setKeyboardTracking(False)
+
         self._mode_selector = QComboBox(self)
         self._mode_selector.setEditable(False)
         self._mode_selector.currentIndexChanged.connect(lambda *_: self._on_control_mode_changed())
