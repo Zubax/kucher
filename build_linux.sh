a084a65eb9867bc3cc898367b898291c8efba96a
loicgilbert <loic.gilbert3@gmail.com>
2019-07-12 09:44:10 +0300

    Update build_linux.sh and .gitmodule

--- build_linux.sh
+++ build_linux.sh
@@ -3,21 +3,22 @@
 # Before running this, make sure all PIP dependencies are installed.
 #
 
-sudo apt-get install patchelf -y
-sudo apt install gcc
-sudo apt-get install scons
+sudo apt-get install -y patchelf
+sudo apt-get install -y gcc
+sudo apt-get install -y scons
 sudo apt-get install -y liblzma-dev
 pip install backports.lzma
 
 pyinstaller --clean --noconfirm pyinstaller.spec || exit 2
-cd kucher/libraries/staticx
+pushd kucher/libraries/staticx
 scons
 python setup.py install
 
 # https://github.com/JonathonReinhart/staticx/issues/79
-cd ../../../dist
+popd
+pushd dist
 mv Kucher Kucher.tmp
 staticx --loglevel DEBUG Kucher.tmp Kucher
 
 rm -rf *.tmp
-cd ..
+popd
